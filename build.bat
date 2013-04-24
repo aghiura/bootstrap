7ec259c05d043b72cc3bb65ec6491d7316dcd51e
ghalex <ghalex@gmail.com>
2013-04-24 01:51:42 -0700

    - Add PhiTodo variables

--- build.bat
+++ build.bat
@@ -2,7 +2,10 @@
 @ECHO OFF
 
 call recess --compile less/bootstrap.less > bootstrap.css
-xcopy bootstrap.css D:\Projects\JavaScript\Poker\css\ /Y
+xcopy bootstrap.css "D:/Projects/JavaScript/PhiTodo/css" /Y
 
 @ECHO ON
 @ECHO Compiling complete.
+
+@ECHO OFF
+pause
